05885051d583771fcca9d8f7434504d45043c829
Melvin-Elian <64053994+Melvin-Elian@users.noreply.github.com>
2020-07-20 18:45:28 -0500

    Add files via upload

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'diego.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fidel.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
