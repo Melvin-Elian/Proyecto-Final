742887c6e3c4134a8edfd15133b9cf1e966eb9d9
Fidel <64174564+Fidel3110@users.noreply.github.com>
2020-07-29 10:11:25 -0500

    Tablas para productos por areas
    creacion de 6 areas para la libreria

---
+++ models.py
@@ -0,0 +1,45 @@
+from django.db import models
+
+# Create your models here.
+class Productos (models.Model):
+    name = models.CharField(max_length=100)
+    img = models.ImageField(upload_to='pics')
+    desc = models.TextField(max_length=200)
+    price = models.IntegerField()
+    offer = models.BooleanField(default=False)
+class Mochilas (models.Model):
+    name = models.CharField(max_length=100)
+    img = models.ImageField(upload_to='pics')
+    desc = models.TextField(max_length=200)
+    price = models.IntegerField()
+    offer = models.BooleanField(default=False)
+class Lapices (models.Model):
+    name = models.CharField(max_length=100)
+    img = models.ImageField(upload_to='pics')
+    desc = models.TextField(max_length=200)
+    price = models.IntegerField()
+    offer = models.BooleanField(default=False)
+class Cuadernos (models.Model):
+    name = models.CharField(max_length=100)
+    img = models.ImageField(upload_to='pics')
+    desc = models.TextField(max_length=200)
+    price = models.IntegerField()
+    offer = models.BooleanField(default=False)
+class Piñateria (models.Model):
+    name = models.CharField(max_length=100)
+    img = models.ImageField(upload_to='pics')
+    desc = models.TextField(max_length=200)
+    price = models.IntegerField()
+    offer = models.BooleanField(default=False)
+class Obsequios (models.Model):
+    name = models.CharField(max_length=100)
+    img = models.ImageField(upload_to='pics')
+    desc = models.TextField(max_length=200)
+    price = models.IntegerField()
+    offer = models.BooleanField(default=False)
+class Juguetes (models.Model):
+    name = models.CharField(max_length=100)
+    img = models.ImageField(upload_to='pics')
+    desc = models.TextField(max_length=200)
+    price = models.IntegerField()
+    offer = models.BooleanField(default=False)
