c397db04c7064d11372b4f4f7baa31dcd2e6dd00
Fidel <64174564+Fidel3110@users.noreply.github.com>
2020-08-04 08:53:09 -0500

    formsJuguetes - formsPiñatria - formsObsequios

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Productos, Cuadernos, Lapices, Mochilas
+from .models import Productos, Cuadernos, Lapices, Mochilas, Juguetes, Piñateria, Obsequios
 
 class ProductosForm(forms.ModelForm):
     class Meta:
@@ -17,4 +17,16 @@ class LapicesForm(forms.ModelForm):
 class MochilasForm(forms.ModelForm):
     class Meta:
         model = Mochilas
+        fields = '__all__'
+class JuguetesForm(forms.ModelForm):
+    class Meta:
+        model = Juguetes
+        fields = '__all__'
+class PiñateriaForm(forms.ModelForm):
+    class Meta:
+        model = Piñateria
+        fields = '__all__'
+class ObsequiosForm(forms.ModelForm):
+    class Meta:
+        model = Obsequios
         fields = '__all__'
